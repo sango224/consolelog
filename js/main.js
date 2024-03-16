04e05f107be7808005ebd965643669f685574b60
sango224 <sango.xx0224@gmail.com>
2024-03-16 15:30:21 +0900

    3/16

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var variableNumber = 1234;
+console.log(variableNumber);
+
+var variableText = "Text";
+console.log(variableText);
+
+var variableBoolean = true;
+console.log(variableBoolean);
+
+var addNumber = 10 + 20;
+console.log(addNumber);
+
+var addNumber = 20 - 10;
+console.log(addNumber);
+
+var addNumber = 50 * 2;
+console.log(addNumber);
+
+var addNumber = 20 / 2;
+console.log(addNumber);
+
+var combinationText = "12" + "34";
+console.log(combinationText);
